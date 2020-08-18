17f394c8750f73fd737068ea01b089c2c2dad928
Matthew Lam <matt@mryanlam.com>
2020-08-18 01:26:07 -0700

    test getting values from sheet

--- validator/validate_sheet.py
+++ validator/validate_sheet.py
@@ -1,14 +1,21 @@
 from typing import Dict
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from itertools import zip_longest
 
 def validate_sheet(uri: str) -> Dict[str, str]:
     scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
     creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
     client = gspread.authorize(creds)
     sheet = client.open_by_url(uri).sheet1
-    records = sheet.get_all_records()
-    print(records)
+    col_1 = sheet.get("B6:B55")
+    col_2 = sheet.get("C6:C55")
+    val = 50
+    for left, right in (zip_longest(col_1, col_2)):
+        print("{} {} {}".format(val, left, right))
+        val -= 1
+
+
 
 if __name__ == '__main__':
     validate_sheet("https://docs.google.com/spreadsheets/d/1ZnGUeif6qvAT9IjgI5QdtCh0lTIuweWaXGeCjVQcF7M/edit?usp=sharing")
