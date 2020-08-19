da23339c5384217b2e02ce6392daa4adda138225
Matthew Lam <matt@mryanlam.com>
2020-08-18 23:52:28 -0700

    implement loading of db for AQ gear

---
+++ validator/populate_db.py
@@ -0,0 +1,42 @@
+from typing import List, Dict
+import gspread
+from create_db import Item
+from db import SolutionLootDB
+import pprint
+
+def populate_item_db(uri: str, raid : str):
+    gc = gspread.service_account(filename='credentials.json')
+    sh = gc.open_by_url(uri).sheet1
+    item_list = list()
+    db = SolutionLootDB()
+    # pp = pprint.PrettyPrinter(indent=4)
+    if raid == "BWL":
+        # loot_list = sh.get("G15:N174")
+        print("BWL is not yet implemented")
+        # TODO: Normalize the classes/specs in prios to match AQ style
+    if raid == "AQ":
+        # Item, Allocation, Type, Designation, First Priority, Second Priority
+        loot_list = sh.get("G15:N131")
+        for row in loot_list:
+            item_list.append(Item(item_name=row[0],
+                                    allocation=row[1],
+                                    item_type=row[2],
+                                    designation=row[3],
+                                    first_prio=row[4],
+                                    second_prio=row[5],
+                                    raid="AQ40"))
+            # item_list.append({"item_name":row[0],
+            #                         "allocation":row[1],
+            #                         "item_type":row[2],
+            #                         "designation":row[3],
+            #                         "first_prio":row[4],
+            #                         "second_prio":row[5],
+            #                         "raid":"AQ40"})
+    # pp.pprint(item_list)
+    db.insert_items(item_list)
+
+if __name__ == '__main__':
+    # AQ40
+    populate_item_db("https://docs.google.com/spreadsheets/d/1gaCOz-wRpey_YghJwYD54ftV-pxx5348O8dPpyn5IcI/edit#gid=0", "AQ")
+    # BWL/MC/ONY
+    populate_item_db("https://docs.google.com/spreadsheets/d/1wbCuQxeZzAOPPmfCXVaYXL-eZh91L5NQxc6lZ0i2Nk0/edit#gid=0", "BWL")
