4648463b71acaa98a42b903b1f69edea1ff2c97b
Matthew Lam <matt@mryanlam.com>
2020-08-22 01:20:00 -0700

    fix some validation on populating items

--- validator/populate_db.py
+++ validator/populate_db.py
@@ -9,7 +9,7 @@ def populate_item_db(uri: str, raid : str):
     sh = gc.open_by_url(uri).sheet1
     item_list = list()
     db = SolutionLootDB()
-    # pp = pprint.PrettyPrinter(indent=4)
+    pp = pprint.PrettyPrinter(indent=4)
     if raid == "BWL":
         # loot_list = sh.get("G15:N174")
         print("BWL is not yet implemented")
@@ -18,12 +18,22 @@ def populate_item_db(uri: str, raid : str):
         # Item, Allocation, Type, Designation, First Priority, Second Priority
         loot_list = sh.get("G15:N131")
         for row in loot_list:
-            item_list.append(Item(item_name=row[0],
+            pp.pprint(row)
+            if len(row) < 8:
+                item_list.append(Item(item_name=row[0],
                                     allocation=row[1],
                                     item_type=row[2],
                                     designation=row[3],
-                                    first_prio=row[4],
-                                    second_prio=row[5],
+                                    first_prio=row[6],
+                                    second_prio=None,
+                                    raid="AQ40"))
+            else:
+                item_list.append(Item(item_name=row[0],
+                                    allocation=row[1],
+                                    item_type=row[2],
+                                    designation=row[3],
+                                    first_prio=row[6],
+                                    second_prio=row[7],
                                     raid="AQ40"))
             # item_list.append({"item_name":row[0],
             #                         "allocation":row[1],
@@ -32,11 +42,11 @@ def populate_item_db(uri: str, raid : str):
             #                         "first_prio":row[4],
             #                         "second_prio":row[5],
             #                         "raid":"AQ40"})
-    # pp.pprint(item_list)
+
     db.insert_items(item_list)
 
 if __name__ == '__main__':
     # AQ40
     populate_item_db("https://docs.google.com/spreadsheets/d/1gaCOz-wRpey_YghJwYD54ftV-pxx5348O8dPpyn5IcI/edit#gid=0", "AQ")
     # BWL/MC/ONY
-    populate_item_db("https://docs.google.com/spreadsheets/d/1wbCuQxeZzAOPPmfCXVaYXL-eZh91L5NQxc6lZ0i2Nk0/edit#gid=0", "BWL")
+    # populate_item_db("https://docs.google.com/spreadsheets/d/1wbCuQxeZzAOPPmfCXVaYXL-eZh91L5NQxc6lZ0i2Nk0/edit#gid=0", "BWL")
